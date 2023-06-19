51aa7bf0b760f6f205edecdc1d0fea0cdbff5d39
duongspm <tranvanduong10a22016bb@gmail.com>
2023-06-19 15:28:44 +0700

    update

---
+++ assets/js/point.js
@@ -0,0 +1,13 @@
+// Schedule Sp
+$(function(){
+    $('.spScroll').bind('touchstart mousedown', function(){
+        $('.swipe').fadeOut();
+    });
+});
+
+// Schedule Sp
+$(function(){
+    $('.spScroll1').bind('touchstart mousedown', function(){
+        $('.swipe1').fadeOut();
+    });
+});
